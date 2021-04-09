38db89e71c35ea6384b80a14846f4000d28f406c
villalpe <villalpe5@gmail.com>
2021-04-09 18:33:24 -0500

    Adding signin and signup page, signin form and button components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/home.component';
-import ShopPage from './pages/shop/shop.component.jsx';
-import Header from './components/header/header.component.jsx';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import './App.css';
 
 
@@ -14,6 +15,7 @@ function App() {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUp} />          
         </Switch>
       </div>
     </Router>
---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './sign-up.styles.scss';
+
+class SignUp extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return(
+            <div>
+            
+            </div>
+        )
+    }
+}
+
+export default SignUp;
